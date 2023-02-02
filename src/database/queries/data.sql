983bad8d6dc03639cfedd5611b10393d0cd79fb4
rcaputto <rgcaputto@gmail.com>
2023-02-02 12:39:18 -0300

    carpeta queries en database

---
+++ src/database/queries/data.sql
@@ -0,0 +1,14 @@
+/*USERS DATA*/
+insert into users (fistName, lastName, email, userName, password, image, newsletter, userCategory_id) values ('Sloan', 'Josselsohn', 'sjosselsohn0@ustream.tv', 'sjosselsohn0', '7ppmMcA', 'http://dummyimage.com/138x100.png/dddddd/000000', false, 1);
+insert into users (fistName, lastName, email, userName, password, image, newsletter, userCategory_id) values ('Alaster', 'Beadle', 'abeadle1@forbes.com', 'abeadle1', 'cZ2UU3ixTOB', 'http://dummyimage.com/200x100.png/cc0000/ffffff', true, 1);
+insert into users (fistName, lastName, email, userName, password, image, newsletter, userCategory_id) values ('Ferdinand', 'Ungerecht', 'fungerecht2@nyu.edu', 'fungerecht2', 'RrDo2B9q', 'http://dummyimage.com/119x100.png/ff4444/ffffff', true, 1);
+insert into users (fistName, lastName, email, userName, password, image, newsletter, userCategory_id) values ('Godard', 'Elsley', 'gelsley3@dmoz.org', 'gelsley3', 'NY3Rv5', 'http://dummyimage.com/161x100.png/ff4444/ffffff', true, 1);
+insert into users (fistName, lastName, email, userName, password, image, newsletter, userCategory_id) values ('Caria', 'Peak', 'cpeak4@google.ca', 'cpeak4', 'uGOQdzWTjeF', 'http://dummyimage.com/238x100.png/5fa2dd/ffffff', true, 1);
+
+/*PRODUCT DATA*/
+
+insert into products (name, quota, image, price) values ('cpaull0', 1, 'http://dummyimage.com/192x100.png/dddddd/000000', '2000');
+insert into products (name, quota, image, price) values ('abim1', 2, 'http://dummyimage.com/204x100.png/cc0000/ffffff', '6000');
+insert into products (name, quota, image, price) values ('vkilshaw2', 3, 'http://dummyimage.com/178x100.png/dddddd/000000', '6000');
+insert into products (name, quota, image, price) values ('dsweedy3', 4, 'http://dummyimage.com/168x100.png/dddddd/000000', '6000');
+insert into products (name, quota, image, price) values ('ejepensen4', 5, 'http://dummyimage.com/124x100.png/cc0000/ffffff', '6000');
